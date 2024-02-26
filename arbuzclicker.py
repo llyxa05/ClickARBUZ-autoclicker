4c47d4708a9c94498c418cb5285134ccfb7b8079
ilyxa05 <77862722+llyxa05@users.noreply.github.com>
2024-02-26 20:21:17 +0100

    Update arbuzclicker.py

--- arbuzclicker.py
+++ arbuzclicker.py
@@ -1,4 +1,5 @@
 import requests
+import time
 import json
 import datetime
 import colorama
@@ -6,28 +7,33 @@ from colorama import Fore, Style
 
 colorama.init()
 urlApiMe = 'https://arbuz.betty.games/api/users/me'
-urlApiClick = 'https://arbuz.betty.games/api/click'
+urlApiClick = 'https://arbuz.betty.games/api/click/apply'
 
 headers = {
     'Content-Type': 'application/json',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-    'Baggage': 'sentry-environment=production,sentry-public_key=6e836760e898018c6059dfcdba712802,sentry-trace_id=a45d9e2c1596467cb19be6d72ee4c391',
+    #'Baggage': 'sentry-environment=production,sentry-public_key=6e836760e898018c6059dfcdba712802,sentry-trace_id=a45d9e2c1596467cb19be6d72ee4c391',
     'accept-language': 'ru-UA,ru;q=0.9,uk-UA;q=0.8,uk;q=0.7,ru-RU;q=0.6,en-US;q=0.5,en;q=0.4',
     'Sec-Ch-Ua': '"Not_A Brand";v="99", "Google Chrome";v="121", Chromium";v="121"',
     'Sec-Ch-Ua-Mobile': '?1',
     'Sec-Ch-Ua-Platform': 'Android',
-    'Sentry-Trace': 'a45d9e2c1596467cb19be6d72ee4c391-83a9f4d27d78842f',
+    #'Sentry-Trace': 'a45d9e2c1596467cb19be6d72ee4c391-83a9f4d27d78842f',
     'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Mobile Safari/537.36',
     'X-Telegram-Init-Data': ('')
-} # ДЛЯ ПРОШАРЕННЫХ. Для более лучшей безопасности (андетекта вас), измените хедеры user-agent, sentry-trace итп на ваши (допустим взяв из вашего смартфона)
+}
+  # в скором времени постараюсь сделать еще более легитимно, посредством получения текущей даты + хеширования ее
+# старый вариант
+#data = {"count": 7}
+#count_as_float =  round(float(data["count"]))
+#energyPerClick = count_as_float #1
 
 while True:
     try:
-        # получаем информацию о пользователе
+        # Получаем информацию о пользователе
         apiMeResponse = requests.get(urlApiMe, headers=headers, timeout=2)
         apiMeResponseJson = apiMeResponse.json()
 
-        # проверяем статус ответа
+        # Проверяем статус ответа
         if apiMeResponse.status_code == 200:
             # разные стринги/зависимости
             username = apiMeResponseJson.get('fullName', 'NO DATA')
@@ -42,12 +48,12 @@ while True:
             else:
                 data = {"count": energyBoostSum}
 
-            # рассчитываем количество возможных кликов
+            # Рассчитываем количество возможных кликов
             current_energy = round(apiMeResponseJson.get('energy', 0.0))
             clicksAvailable = current_energy // energyBoostSum # старый вариант energyPerClick
 
             #print(f'[DEBUG] current_energy: {current_energy} | energyBoostSum: {energyBoostSum}')
-            # выполняем клики в соответствии с доступной энергией
+            # Выполняем клики в соответствии с доступной энергией
             for _ in range(clicksAvailable):
                 apiClickResponse = requests.post(urlApiClick, json=data, headers=headers, timeout=2)
                 apiClickResponseJson = apiClickResponse.json()
@@ -61,7 +67,7 @@ while True:
                     print(f'{Fore.LIGHTYELLOW_EX}[{Fore.LIGHTBLACK_EX}({current_time}){Fore.LIGHTWHITE_EX} {Fore.LIGHTBLUE_EX}CLICKED{Fore.LIGHTYELLOW_EX}]{Fore.LIGHTWHITE_EX} User: {Fore.GREEN}{username}{Style.RESET_ALL} {Fore.LIGHTYELLOW_EX}|{Fore.LIGHTWHITE_EX} Balance: {Fore.GREEN}{balance}{Fore.RED}🍉 {Fore.LIGHTYELLOW_EX}| {Fore.LIGHTWHITE_EX}Energy: {Fore.LIGHTBLUE_EX}{current_energy}{Fore.LIGHTYELLOW_EX}⚡️ | {Fore.LIGHTWHITE_EX}PerClick: {Fore.GREEN}{apiClickArbuzCount}{Fore.RED}🍉  {Fore.LIGHTYELLOW_EX}| {Fore.LIGHTWHITE_EX}PerRequest: {Fore.GREEN}{arbuzPerRequest}{Fore.RED}🍉')
                 else:
                     print(f'[ERROR] {apiClickResponse.status_code} {apiClickResponse.text}')
-                    break  # прекращаем клики если произошла ошибка
+                    break  # Прекращаем клики, если произошла ошибка
 
         #print(f'{apiMeResponse.text}')
         else:
